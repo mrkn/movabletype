mrkn/movabletype

commit 1320c295e81fbffcf76d90353b2b65af7e21b089

Respect the master switch to on/off module caching in the Manage Templates screen.  BugId:102535
git-svn-id: http://code.sixapart.com/svn/movabletype/branches/greyhound@4502 3860aeb0-bd15-0410-af20-b8d1745f506e

lib/MT/CMS/Template.pm
@@ -659,10 +659,10 @@ sub list {
         my ( $obj, $row ) = @_;
         my $template_type;
         my $type = $row->{type} || '';
+        my $tblog = $obj->blog_id == $blog->id ? $blog : MT::Blog->load( $obj->blog_id );
         if ( $type =~ m/^(individual|page|category|archive)$/ ) {
             $template_type = 'archive';
             # populate context with templatemap loop
-            my $tblog = $obj->blog_id == $blog->id ? $blog : MT::Blog->load( $obj->blog_id );
             if ($tblog) {
                 $row->{archive_types} = _populate_archive_loop( $app, $tblog, $obj );
             }
@@ -685,7 +685,7 @@ sub list {
         else {
             $template_type = 'system';
         }
-        $row->{use_cache} = ( ($obj->cache_expire_type || 0) != 0 ) ? 1 : 0;
+        $row->{use_cache} = ( $tblog->include_cache && ($obj->cache_expire_type || 0) != 0 )  ? 1 : 0;
         $row->{template_type} = $template_type;
         $row->{type} = 'entry' if $type eq 'individual';
         my $published_url = $obj->published_url;
@@ -1674,7 +1674,6 @@ sub build_template_table {
           if $row->{name} eq '';
         my $published_url = $tmpl->published_url;
         $row->{published_url} = $published_url if $published_url;
-        $row->{use_cache} = ( ($tmpl->cache_expire_type || 0) != 0 )  ? 1 : 0;
 
         # FIXME: enumeration of types
         $row->{can_delete} = 1

commit cc8bd3de4e795df7ba170f621b5b1332188b8310

Blog may be null if it was the list of global templates.  Fixed a bug introduced in r4502.
git-svn-id: http://code.sixapart.com/svn/movabletype/branches/greyhound@4507 3860aeb0-bd15-0410-af20-b8d1745f506e

lib/MT/CMS/Template.pm
@@ -659,7 +659,7 @@ sub list {
         my ( $obj, $row ) = @_;
         my $template_type;
         my $type = $row->{type} || '';
-        my $tblog = $obj->blog_id == $blog->id ? $blog : MT::Blog->load( $obj->blog_id );
+        my $tblog = MT::Blog->load( $obj->blog_id ) if $obj->blog_id;
         if ( $type =~ m/^(individual|page|category|archive)$/ ) {
             $template_type = 'archive';
             # populate context with templatemap loop
@@ -685,8 +685,8 @@ sub list {
         else {
             $template_type = 'system';
         }
-        $row->{use_cache} = ( $tblog->include_cache && ($obj->cache_expire_type || 0) != 0 )  ? 1 : 0;
-        $row->{use_ssi} = ( $tblog->include_system && $obj->include_with_ssi )  ? 1 : 0;
+        $row->{use_cache} = ( $tblog && $tblog->include_cache && ($obj->cache_expire_type || 0) != 0 )  ? 1 : 0;
+        $row->{use_ssi} = ( $tblog && $tblog->include_system && $obj->include_with_ssi )  ? 1 : 0;
         $row->{template_type} = $template_type;
         $row->{type} = 'entry' if $type eq 'individual';
         my $published_url = $obj->published_url;